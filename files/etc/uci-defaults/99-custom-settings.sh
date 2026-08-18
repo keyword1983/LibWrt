599c6663caf0a4bbe5018d8a9c375fc665305130
keyword1983 <frank.keyword@gmail.com>
2026-08-18 21:27:18 +0800

    perf: add 5 enterprise TCP sysctl optimizations (port range 1024-65535, tw_reuse, syn_backlog 8192, optmem_max 200k, rfc1337)

--- files/etc/uci-defaults/99-custom-settings.sh
+++ files/etc/uci-defaults/99-custom-settings.sh
@@ -49,7 +49,7 @@ if [ -f /etc/config/zram ]; then
     uci commit zram
 fi
 
-# 5. Network Performance & TCP BBR Tuning
+# 5. Network Performance & Enterprise TCP Tuning
 cat << "EOF" >> /etc/sysctl.conf
 vm.swappiness=60
 net.core.default_qdisc=fq
@@ -64,6 +64,14 @@ net.core.netdev_max_backlog=10000
 net.ipv4.tcp_slow_start_after_idle=0
 net.ipv4.tcp_rmem=4096 87380 16777216
 net.ipv4.tcp_wmem=4096 65536 16777216
+net.ipv4.ip_local_port_range=1024 65535
+net.ipv4.tcp_tw_reuse=1
+net.ipv4.tcp_max_syn_backlog=8192
+net.ipv4.tcp_synack_retries=2
+net.core.optmem_max=204800
+net.core.rmem_max=16777216
+net.core.wmem_max=16777216
+net.ipv4.tcp_rfc1337=1
 EOF
 sysctl -p /etc/sysctl.conf 2>/dev/null
 
